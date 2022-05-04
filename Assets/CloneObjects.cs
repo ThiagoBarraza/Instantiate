215a505d862acda2c18fc76c013ed3784fddf9f3
ThiagoBarraza <46645966@est.ort.edu.ar>
2022-05-04 15:49:53 -0300

    Initial commit

---
+++ Assets/CloneObjects.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneObjects : MonoBehaviour
+{
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public GameObject Cubo;
+
+    int i;
+
+    public void CloneObject()
+    {
+        
+
+        while (i != 3)
+        {
+            Instantiate(Cubo);
+
+            i++;
+        }
+
+        i = 0;
+
+
+
+        
+    }
+
+    
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Instantiate
+ Clase 4/5
